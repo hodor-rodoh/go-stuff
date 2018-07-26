d64fb9d7d78e9bbf0860f539f24243a08dcd3b88
Sean Shen <32344792+hodor-rodoh@users.noreply.github.com>
2018-07-26 12:29:47 -0500

    Update parse2.go

--- parse2.go
+++ parse2.go
@@ -15,42 +15,6 @@ func main() {
 	var allRecords []Record
 
 	input := []byte(`[
-    {
-      "app": "infradocs-qa-goromasamu",
-      "namespace": "nozomi"
-    },
-    {
-      "app": "infradocs-qa-telematicsct",
-      "namespace": "nozomi"
-    },
-    {
-      "app": "infradocs-tsp-qa",
-      "namespace": "nozomi"
-    },
-    {
-      "app": "pghttpbin-qa-goromasamu",
-      "namespace": "nozomi"
-    },
-    {
-      "app": "pghttpbin-qa-telematicsct",
-      "namespace": "nozomi"
-    },
-    {
-      "app": "pghttpbin-tsp-qa",
-      "namespace": "nozomi"
-    },
-    {
-      "app": "pipelinedemo-qa-goromasamu",
-      "namespace": "nozomi"
-    },
-    {
-      "app": "pipelinedemo-qa-telematicsct",
-      "namespace": "nozomi"
-    },
-    {
-      "app": "pipelinedemo-tsp-qa",
-      "namespace": "nozomi"
-    }
   ]`)
 
 	var tmpRecords []Record
