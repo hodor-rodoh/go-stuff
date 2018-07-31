959e8912099bb3742fc7b82ae6999559f7292b79
Sean Shen <sean.shen@toyotaconnected.com>
2018-07-31 13:07:59 -0500

    added regexp kind of works

--- parse.go
+++ parse.go
@@ -8,41 +8,47 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	//"regexp"
+	"regexp"
 )
 // go get k8s.io/client-go/*version#
 // kubectl --context am562-kube0 get ing -n nozomi -o json
 
-// type output struct {
-//   output []metadata
-// }
-
-type metadata struct {
+type metadata struct { //struct for app and namespace for outputs
 	App       string //`json:"app"`
 	Namespace string //`json:"namespace"`
 }
 
+type tinder struct { //struct for my regexp matches
+	App 			string
+	Namespace string
+}
+
 func main() {
-	jsonFile, err := os.Open("output.json")
+	jsonFile, err := os.Open("output.json") 	// opens output.json file prints error with err
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println("opened dat file homes!")
+	fmt.Println("opened dat file homes!") 	// confirmation that file is opened
 
-	defer jsonFile.Close()
+	defer jsonFile.Close() 	//close file
 
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 
 	var info []metadata
 
-	json.Unmarshal(([]byte(byteValue)), &info)
+	json.Unmarshal(([]byte(byteValue)), &info) // Unmarshal jsonFile to parse and shit
 	// fmt.Println(info)
 
-	for i := 0; i < len(info); i++ {
+	for i := 0; i < len(info); i++ { // loop through all the stuff
 		// fmt.Println(info[i].App + " " + info[i].Namespace)
-		// add a regexp.MatchString for .App (`telematicsct`,info)
-		fmt.Println(info[i])
-	}
+		// fmt.Println(info[i])
 
+		match := (info[i].App + " " + info[i].Namespace)
+		// fmt.Println(match)
+
+		re := regexp.MustCompile("-telematicsct") // must contain -telematicsct
+		fmt.Printf("%q\n", re.FindStringSubmatch(match)) // finds telematicsct in a substring and returns
+
+	}
 }
