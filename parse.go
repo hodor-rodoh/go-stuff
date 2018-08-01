09e9f1ba033e96848b159881cc7df0b189200bd7
Sean Shen <32344792+hodor-rodoh@users.noreply.github.com>
2018-08-01 17:02:42 -0500

    cleaned it up

--- parse.go
+++ parse.go
@@ -10,39 +10,38 @@ import (
 	"regexp"
 	"strings"
 )
-// go get k8s.io/client-go/*version#
-// kubectl --context am562-kube0 get ing -n nozomi -o json | jq -r '[.items[] | { app: .metadata.name, namespace: .metadata.namespace }]' > output.json
-// kubectl --context am562-kube0 get secrets -n nozomi -o json | jq -r '[.items[] | { app: .metadata.name, namespace: .metadata.namespace }]' > output.json
 
-type metadata struct { //struct for app and namespace for outputs
-	App       string //`json:"app"`
-	Namespace string //`json:"namespace"`
+// kubectl --context am360-kube0 get ing --all-namespaces -o json | jq -r '[.items[] | { app: .metadata.name, namespace: .metadata.namespace }]' > output.json
+// kubectl --context am360-kube0 get secrets --all-namespaces -o json | jq -r '[.items[] | { app: .metadata.name, namespace: .metadata.namespace }]' > output.json
+
+type metadata struct {
+	App       string
+	Namespace string
 }
 
-// todo break this shit out into different functions
 func main() {
-	jsonFile, err := os.Open("output.json") 	// opens output.json file prints error with err
+	jsonFile, err := os.Open("output.json")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	defer jsonFile.Close() 	//close file
+	defer jsonFile.Close()
 
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 
 	var info []metadata
 	var results []metadata
 
-	json.Unmarshal(([]byte(byteValue)), &info) // Unmarshal jsonFile to parse and shit
+	json.Unmarshal(([]byte(byteValue)), &info)
 
-	for _, app := range info { // loop through all the stuff
+	for _, app := range info {
 		match, _ := regexp.MatchString("telematicsct", app.App)
 		if match {
 			results = append(results, app)
-			// hodor := fmt.Sprintf("kubectl --context am562-kube0 get ingress %s --namespace %s", app.App, app.Namespace) // to get ingress for testing
-			// hodor := fmt.Sprintf("kubectl --context am562-kube0 --namespace %s delete ingress/%s \n", app.Namespace, app.App) // deleting ingress
-			hodor := fmt.Sprintf("kubectl --context am562-kube0 get secrets %s --namespace %s", app.App, app.Namespace) // to get secrets for testing
-			// hodor := fmt.Sprintf("kubectl --context am562-kube0 --namespace %s delete secret/%s \n", app.Namespace, app.App) // deleting ingress
+			// hodor := fmt.Sprintf("kubectl --context am360-kube0 get ingress %s --namespace %s", app.App, app.Namespace)
+			// hodor := fmt.Sprintf("kubectl --context am360-kube0 --namespace %s delete ingress/%s \n", app.Namespace, app.App)
+			// hodor := fmt.Sprintf("kubectl --context am360-kube0 get secrets %s --namespace %s", app.App, app.Namespace)
+			hodor := fmt.Sprintf("kubectl --context am360-kube0 --namespace %s delete secret/%s \n", app.Namespace, app.App)
 			fmt.Println(hodor)
 			tokens := strings.Fields(hodor)
 			executable := tokens[0]
