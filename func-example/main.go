7d6f04a9e85ecd7e50dbbf57ff422d2abe09596f
Sean Shen <sean.shen@toyotaconnected.com>
2018-08-06 16:09:49 -0500

    changes

--- func-example/main.go
+++ func-example/main.go
@@ -1,45 +1,63 @@
 package main
 
 import (
-  // "encoding/json"
-  // "io/ioutil"
-  // "regexp"
+  "encoding/json"
+  "io/ioutil"
+  "regexp"
   "os"
   "os/exec"
   "log"
-  // "fmt"
-  // "strings"
+  "fmt"
+  "strings"
 )
 
-// func getData() {
-//   hodor := fmt.Sprintf("kubectl --context am560-kube0 get ingress --all-namespaces -o json")
-//   // rodoh := fmt.Sprintf(" | jq -r '[.items[] | { app: .metadata.app, namespace: .metadata.namespace }]'")
-//   // butts := hodor + rodoh
-//   fmt.Println(hodor)
-//   tokens := strings.Fields(hodor)
-//   executable := tokens[0]
-//   args := tokens[1:len(tokens)]
-//
-//   cmd := exec.Command(executable, args...)
-//   cmd.Stdout = os.Stdout
-//   cmd.Stderr = os.Stderr
-//   err := cmd.Run()
-//   if err != nil {
-//     log.Fatal(err)
-//   }
-// }
+type metadata struct {
+	App       string
+	Namespace string
+}
 
-func helloTest() {
-  cmd := exec.Command("sh", "-c", "kubectl --context am560-kube0 get ingress -n nozomi -o json")
-  cmd.Stdout = os.Stdout
-  cmd.Stderr = os.Stderr
-  err := cmd.Run()
+var info []metadata
+var results []metadata
+
+func openFile() {
+  jsonFile, err := os.Open("output.json")
   if err != nil {
-    log.Fatal(err)
+    fmt.Println(err)
+  }
+
+  defer jsonFile.Close()
+
+  byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	json.Unmarshal(([]byte(byteValue)), &info)
+}
+
+func searchData() {
+
+  for _, app := range info {
+    match, _ := regexp.MatchString("telematicsct", app.App)
+    if match {
+      results = append(results, app)
+      hodor := fmt.Sprintf("kubectl --context am160-kube0 get ingress %s --namespace %s", app.App, app.Namespace)
+      // hodor := fmt.Sprintf("kubectl --context am160-kube0 --namespace %s delete ingress/%s \n", app.Namespace, app.App)
+      // hodor := fmt.Sprintf("kubectl --context am160-kube0 get secrets %s --namespace %s", app.App, app.Namespace)
+      // hodor := fmt.Sprintf("kubectl --context am160-kube0 --namespace %s delete secret/%s \n", app.Namespace, app.App)
+      fmt.Println(hodor)
+      tokens := strings.Fields(hodor)
+      executable := tokens[0]
+      args := tokens[1:len(tokens)]
+      cmd := exec.Command(executable, args...)
+      cmd.Stdout = os.Stdout
+      cmd.Stderr = os.Stderr
+      err := cmd.Run()
+      if err != nil {
+        log.Fatal(err)
+      }
+    }
   }
 }
 
 func main() {
-  // getData()
-  helloTest()
+  openFile()
+  searchData()
 }
